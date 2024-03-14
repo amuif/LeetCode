0e57f637b1e89984e42e831a018f483e66b7e9f5
amuif <69429140+amuif@users.noreply.github.com>
2024-03-14 22:14:29 +0300

    Create List Comprehensions.py

---
+++ List Comprehensions.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+    # Using list comprehension to generate the list of coordinates
+    coordinates = [[i, j, k] for i in range(x + 1) for j in range(y + 1) for k in range(z + 1) if i + j + k != n]
+    print(coordinates)
