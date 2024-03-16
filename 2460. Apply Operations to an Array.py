d027588d64f25be2d9f1347e01db4bacb4cec7c4
amuif <69429140+amuif@users.noreply.github.com>
2024-03-16 15:50:28 +0300

    Create 2460. Apply Operations to an Array.py

---
+++ 2460. Apply Operations to an Array.py
@@ -0,0 +1,16 @@
+class Solution:
+    def applyOperations(self, nums: List[int]) -> List[int]:
+
+        for i in range(len(nums) - 1):
+            if nums[i] == nums[i + 1]:
+                nums[i] *= 2
+                nums[i + 1] = 0
+        count = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[count] = nums[i]
+                count += 1
+        for i in range(count, len(nums)):
+            nums[i] = 0
+        
+        return nums
