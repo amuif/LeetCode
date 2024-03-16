41c08f0c3ac0850ebeb80206f251ee71a62f21ee
amuif <69429140+amuif@users.noreply.github.com>
2024-03-16 10:51:28 +0300

    Create Set .union() Operation.py

---
+++ Set .union() Operation.py
@@ -0,0 +1,8 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+eng = int(input())
+engSub = set(map(int, input().split()))
+french = int(input())
+frenchSub = set(map(int, input().split()))
+
+
+print(len(engSub.union(frenchSub)))
