230ae72f219fefa10854fd41b9f457d3953a5a51
amuif <69429140+amuif@users.noreply.github.com>
2024-04-22 22:26:03 +0300

    Time: 717 ms (5.13%), Space: 30.4 MB (49.93%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.py
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.py
@@ -1,6 +1,5 @@
 class Solution:
     def maxIceCream(self, costs: List[int], coins: int) -> int:
-        # 1 - 5 [1, 2, 0, 0, 2] -> [1, 2, 2, 5, 5]
         __max = max(costs)
 
         count = [0]*(__max + 1)
@@ -8,9 +7,6 @@ class Solution:
         for i in range(len(costs)):
             count[costs[i]] += 1
         
-        new = []
-        for i in range(__max):
-            new += [i + 1]*count[i + 1]
 
         counted = 0 
         for i in range(1, 1 + __max):
