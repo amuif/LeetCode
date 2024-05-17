981c02d4c320cb26ae5e6fc94a98e1ce91df2965
amuif <69429140+amuif@users.noreply.github.com>
2024-05-17 16:36:34 +0300

    Time: 36 ms (46.37%), Space: 16.5 MB (87.19%) - LeetHub

---
+++ 0856-score-of-parentheses/0856-score-of-parentheses.py
@@ -0,0 +1,16 @@
+class Solution:
+    def scoreOfParentheses(self, s: str) -> int:
+        stack = []
+        count =0
+        for i in s:
+            if i == "(":
+                stack.append(count)
+                count = 0
+
+            else:
+                
+                count = stack.pop() + max(2*count , 1)
+
+       
+
+        return count
