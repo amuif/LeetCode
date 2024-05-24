a8ad188263333fdf8cdf06685a47deb61971f715
I <69429140+amuif@users.noreply.github.com>
2024-05-24 17:51:08 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0860-lemonade-change/0860-lemonade-change.py
+++ 0860-lemonade-change/0860-lemonade-change.py
@@ -1,29 +1,24 @@
 class Solution:
     def lemonadeChange(self, bills: List[int]) -> bool:
-        # Initialize counters for five and ten dollar bills
-        five_dollar_count = ten_dollar_count = 0
-      
-        # Iterate over each bill received
-        for bill in bills:
-            if bill == 5:
-                # If it's a $5 bill, simply increase the count of $5 bills
-                five_dollar_count += 1
-            elif bill == 10:
-                # If it's a $10 bill, give one $5 bill as change
-                ten_dollar_count += 1
-                five_dollar_count -= 1
-            else:
-                # If it's a $20 bill, try to give one $10 and one $5 as change if possible
-                # Otherwise, give three $5 bills as change
-                if ten_dollar_count:
-                    ten_dollar_count -= 1
-                    five_dollar_count -= 1
+        five_count = 0
+        ten_count = 0
+        for i in bills:
+            if i == 5:
+                five_count +=1
+            elif i == 10:
+                ten_count +=1
+                five_count -=1
+            elif i == 20:
+                if ten_count:
+                    ten_count -=1
+                    five_count -=1
                 else:
-                    five_dollar_count -= 3
-          
-            # If at any point the count of $5 bills drops below zero, it's impossible to give change
-            if five_dollar_count < 0:
+                    five_count -3
+            if five_count < 0:
                 return False
-      
-        # If we got to the end without running out of $5 bills, we can give change for all transactions
+    
         return True
+                
+            
+
+        
