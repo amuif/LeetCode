0da2ec03678a6a6dd2287e6219159376b51d1575
I <69429140+amuif@users.noreply.github.com>
2024-07-10 11:15:57 +0300

    Time: 73 ms (51.54%), Space: 20.2 MB (19.51%) - LeetHub

--- 0303-range-sum-query-immutable/0303-range-sum-query-immutable.py
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.py
@@ -2,7 +2,12 @@ from itertools import accumulate
 
 class NumArray:
     def __init__(self, nums: List[int]):
-        self.cumulative_sum = list(accumulate(nums, initial=0))
-
+        self.prefix_sum= [0]* (len(nums)+1)
+        for i in range(1,len(nums)+1):
+            self.prefix_sum[i] = self.prefix_sum[i-1] + nums[i-1]
+        
     def sumRange(self, left: int, right: int) -> int:
-        return self.cumulative_sum[right + 1] - self.cumulative_sum[left]
+        
+        return self.prefix_sum[right+1] - self.prefix_sum[left]
+        
+        
