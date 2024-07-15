37af80e8ac7fd38fe62cf69803ec014c066d5f06
I <69429140+amuif@users.noreply.github.com>
2024-07-15 15:53:35 +0300

    Time: 8447 ms (5.01%), Space: 16.3 MB (89.78%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,16 +1,15 @@
 class Solution:
     def fib(self, n: int) -> int:
         output = 0
+
         def fibonacci(n: int) -> int:
-            
+
             if n <= 1:
                 return n
             else:
-                return fibonacci(n-1) + fibonacci(n-2)
+                return fibonacci(n - 1) + fibonacci(n - 2)
 
-        # Test the function
-        for i in range(10):
-            output =fibonacci(n)
-            
+        for i in range(n):
+            output = fibonacci(n)
+
         return output
-            
