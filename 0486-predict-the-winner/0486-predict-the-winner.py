1361bc1a5d5f879cfc7d6efcba56c90a747e5daf
I <69429140+amuif@users.noreply.github.com>
2024-07-16 14:44:03 +0300

    Time: 2335 ms (15.06%), Space: 16.5 MB (47.36%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -0,0 +1,11 @@
+class Solution:
+    def predictTheWinner(self, nums: List[int]) -> bool:
+        n = len(nums)
+        def dfs(i, j):
+            if i == j:
+                return nums[i]
+            leftDiff = nums[i] - dfs(i + 1, j)
+            rightDiff = nums[j] - dfs(i, j - 1)
+            return max(leftDiff, rightDiff)
+        
+        return dfs(0, n - 1) >= 0
