cd19af312b68255a15ae79d16622b1270b734a3d
I <69429140+amuif@users.noreply.github.com>
2024-09-09 01:47:31 -0700

    Time: 153 ms (62.14%), Space: 18.8 MB (60.74%) - LeetHub

---
+++ 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.py
@@ -0,0 +1,8 @@
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        output = []
+        for i in nums:
+            output.append(i*i)
+        output.sort()
+        return output
+        
