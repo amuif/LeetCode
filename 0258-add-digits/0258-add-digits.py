7a3b993d9d6532af351290bf8498f67366cb7c33
I <69429140+amuif@users.noreply.github.com>
2024-11-06 09:12:13 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0258-add-digits/0258-add-digits.py
+++ 0258-add-digits/0258-add-digits.py
@@ -1,7 +1,15 @@
 class Solution:
     def addDigits(self, num: int) -> int:
-        if num == 0:
-            return 0
+        if num < 10:
+            return num
 
-            
-        return (num - 1) % 9 + 1
+        sumi = 0
+
+        while num > 9:
+            for i in str(num):
+                sumi += int(i)
+            if sumi < 9:
+                return sumi
+            num = sumi
+            sumi = 0
+        return sumi
