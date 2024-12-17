7de143b1e3e15442b8ed343809464e238a572c40
I <69429140+amuif@users.noreply.github.com>
2024-12-18 00:38:27 +0300

    Time: 3 ms (41.11%), Space: 18 MB (7.54%) - LeetHub

--- 1122-relative-sort-array/1122-relative-sort-array.py
+++ 1122-relative-sort-array/1122-relative-sort-array.py
@@ -1,3 +1,18 @@
 class Solution:
     def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
-        return sorted(arr1, key=lambda x: (arr2.index(x) if x in arr2 else len(arr2) + x))
+        output = []
+        arr1_counted = Counter(arr1)
+        print(arr1_counted)
+        not_in_arr2 = []
+
+        for i in arr1:
+            if i not in arr2:
+                not_in_arr2.append(i)
+        not_in_arr2.sort()
+
+        for i in range(len(arr2)):
+            count = arr1_counted.get(arr2[i], 0)
+            output.append([arr2[i]] * count)
+        flat = sum(output,[])
+
+        return  flat + not_in_arr2
