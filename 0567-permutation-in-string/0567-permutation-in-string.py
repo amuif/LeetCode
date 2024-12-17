512e977ebad1e94fdfc05f6bd92743694fe1c5d2
I <69429140+amuif@users.noreply.github.com>
2024-12-17 23:26:42 +0300

    Time: 1107 ms (17%), Space: 18.1 MB (5.95%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,39 +1,15 @@
-from collections import Counter
 class Solution:
-    def checkInclusion(self, pattern: str, text: str) -> bool:
-        pattern_length, text_length = len(pattern), len(text)
-      
-        if pattern_length > text_length:
-            return False
-      
-        char_counter = Counter()
-      
-        for pattern_char, text_char in zip(pattern, text[:pattern_length]):
-            char_counter[pattern_char] -= 1
-            char_counter[text_char] += 1
-      
-        diff_count = sum(x != 0 for x in char_counter.values())
-      
-        if diff_count == 0:
-            return True
-      
-        for i in range(pattern_length, text_length):
-            char_out = text[i - pattern_length]
-            char_in = text[i]
-          
-            if char_counter[char_in] == 0:
-                diff_count += 1
-            char_counter[char_in] += 1
-            if char_counter[char_in] == 0:
-                diff_count -= 1
-          
-            if char_counter[char_out] == 0:
-                diff_count += 1
-            char_counter[char_out] -= 1
-            if char_counter[char_out] == 0:
-                diff_count -= 1
-          
-            if diff_count == 0:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        s1_count = Counter(s1)
+
+        l = 0
+        r = len(s1) - 1
+
+        while r < len(s2):
+            if s1_count == Counter(s2[l:r+ 1]):
                 return True
-      
+            l += 1
+            r += 1
+
         return False
+        
