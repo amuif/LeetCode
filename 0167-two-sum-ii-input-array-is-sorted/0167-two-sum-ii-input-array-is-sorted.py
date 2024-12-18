b2045ac955ad5a35329b9e708249739d2555173a
I <69429140+amuif@users.noreply.github.com>
2024-12-18 07:54:20 +0300

    Time: 8 ms (11.6%), Space: 17.6 MB (54.04%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -14,8 +14,8 @@ class Solution:
             if numbers[l]+numbers[r]==target:
                 return [l+1,r+1]
             if numbers[l]+numbers[r]>target:
-                r-= 1
+                r-=1
             if numbers[l]+numbers[r]<target:
-                l+= 1
+                l+=1
 
         
