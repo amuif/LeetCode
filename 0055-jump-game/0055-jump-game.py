c1240ee3d96348a71874564484dea530190801f4
I <69429140+amuif@users.noreply.github.com>
2025-03-05 16:10:16 +0300

    Time: 31 ms (48.53%), Space: 18.5 MB (73.6%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -3,8 +3,10 @@ class Solution:
         farthest = 0
         for i in range(len(nums)):
             if i > farthest:
-                return False 
-            farthest = max(farthest, i + nums[i])
-            if farthest >= len(nums) - 1:
-                return True  
+                return False
+
+            farthest = max(farthest,i+nums[i])
+            if farthest >= len(nums)-1:
+                return True
+        
         return False
