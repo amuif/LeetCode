54992d87b60803a7eb8388778d1ea91d7cfc72be
I <69429140+amuif@users.noreply.github.com>
2025-03-07 16:23:57 +0300

    Time: 87 ms (82.14%), Space: 27.6 MB (55.67%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -1,15 +1,14 @@
-from typing import List
-
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        stack_temp = [(temperatures[-1], len(temperatures)-1)]
-        ans = [0] * len(temperatures)
+        mono = []
+        output = [0] * (len(temperatures))
 
-        for i in range(len(temperatures)-2, -1, -1):
-            while stack_temp and temperatures[i] >= stack_temp[-1][0]:
-                stack_temp.pop()
-            if stack_temp:
-                ans[i] = stack_temp[-1][1] - i
-            stack_temp.append((temperatures[i], i))
+        for i in range(len(temperatures)):
 
-        return ans
+            while mono and temperatures[i] > temperatures[mono[-1]]:
+                index = mono.pop()
+                output[index] = i - index
+                
+            mono.append(i)
+
+        return output
